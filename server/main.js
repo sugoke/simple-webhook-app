000272fded398ed9ea8d0d88a6b8bf20b6b4b132
sugoke <sugoke@hotmail.com>
2023-04-11 15:59:46 +0200

    Initial commit

--- server/main.js
+++ server/main.js
@@ -1,21 +1,40 @@
 import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
+import { HTTP } from 'meteor/http';
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const callChatGPT = async (prompt) => {
+  const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+  try {
+    const response = await HTTP.call('POST', apiUrl, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${OPENAI_API_KEY}`,
+      },
+      data: {
+        prompt,
+        max_tokens: 50,
+        n: 1,
+        stop: null,
+        temperature: 1,
+      },
+    });
+
+    if (response && response.data && response.data.choices && response.data.choices.length > 0) {
+      return response.data.choices[0].text.trim();
+    } else {
+      throw new Error('Invalid response from ChatGPT API');
+    }
+  } catch (error) {
+    console.error('Error calling ChatGPT API:', error);
+    throw new Error('Failed to fetch response from ChatGPT API');
+  }
+};
 
 Meteor.startup(() => {
-  // Code to run on server at startup
-
-  WebApp.connectHandlers.use('/', (req, res, next) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Welcome to the MeteorJS app!');
-  } else {
-    next();
-  }
-});
-
-
-  // Define a middleware function to handle incoming POST requests
-  WebApp.connectHandlers.use('/post-endpoint', (req, res, next) => {
+  WebApp.connectHandlers.use('/post-endpoint', async (req, res, next) => {
     if (req.method === 'POST') {
       let body = '';
 
@@ -23,16 +42,22 @@ Meteor.startup(() => {
         body += chunk.toString();
       });
 
-      req.on('end', () => {
+      req.on('end', async () => {
         console.log('Received POST data:', body);
 
-        // Send a response
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('POST request received');
+        // Call the ChatGPT API with the received data as the prompt
+        try {
+          const chatGptResponse = await callChatGPT(body);
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(chatGptResponse);
+        } catch (error) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Error fetching response from ChatGPT API');
+        }
       });
     } else {
-      // Allow other request methods to pass through
-      next();
+      res.writeHead(405, { 'Content-Type': 'text/plain' });
+      res.end('Method Not Allowed');
     }
   });
 });
