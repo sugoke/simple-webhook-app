7474d33509d9152db0beb51d67b5fccf465c42bb
sugoke <sugoke@hotmail.com>
2023-04-11 10:36:51 +0200

    Initial commit

--- server/main.js
+++ server/main.js
@@ -4,6 +4,16 @@ import { WebApp } from 'meteor/webapp';
 Meteor.startup(() => {
   // Code to run on server at startup
 
+  WebApp.connectHandlers.use('/', (req, res, next) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to the MeteorJS app!');
+  } else {
+    next();
+  }
+});
+
+
   // Define a middleware function to handle incoming POST requests
   WebApp.connectHandlers.use('/post-endpoint', (req, res, next) => {
     if (req.method === 'POST') {
